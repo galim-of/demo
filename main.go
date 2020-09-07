b96f1b024c204b7e790d252f56d3955e536d78e8
Azamat <galim-of@users.noreply.github.com>
2020-09-07 10:33:14 +0500

    Remove hardcoded port to use with Heroku

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 	"demo/web"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
 	http.HandleFunc("/auth", web.Authenticate)
 	http.HandleFunc("/register", web.Register)
 	http.HandleFunc("/refresh", web.Refresh)
 	http.HandleFunc("/delete", web.Delete)
 	http.HandleFunc("/deleteAll", web.DeleteAll)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 
 	defer func() {
 		if err := dbl.Close(); err != nil {
